942786de747cbea6d940ca149231ea135dba90a3
jcbusche <jcbusche@gmail.com>
2013-12-25 15:46:33 -0500

    dealing with url problem in AJAX call

--- js/read.js
+++ js/read.js
@@ -16,7 +16,7 @@ $('.display-btn').click(function(){
 
         $.ajax({
             type: 'POST',
-            url: '../readings/r_read',
+            url: 'p4.johnbusche.com/readings/r_read',
 
             success: function(response) {
 
