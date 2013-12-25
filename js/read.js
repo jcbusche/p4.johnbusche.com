32b71e6cd104e077f948182d0d03b22793f3e1e6
jcbusche <jcbusche@gmail.com>
2013-12-25 15:50:20 -0500

    fixing ajax url problem

--- js/read.js
+++ js/read.js
@@ -16,7 +16,7 @@ $('.display-btn').click(function(){
 
         $.ajax({
             type: 'POST',
-            url: 'p4.johnbusche.com/readings/r_read',
+            url: '../readings/r_read',
 
             success: function(response) {
 
