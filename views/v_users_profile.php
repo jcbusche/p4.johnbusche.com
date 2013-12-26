bf379ebc2c583e57947fa4777a5a533fcb18ead4
jcbusche <jcbusche@gmail.com>
2013-12-25 21:06:21 -0500

    see above

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -55,7 +55,7 @@
 			    <p><?=$post['title']?></p>
 
 			    <script>
-			    	var words = String(<?=$post['content']?>);
+			    	var words = String(<?php echo $post['content']?>);
 			    </script>			  		   
 
 			    <time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
