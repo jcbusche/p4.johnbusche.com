8ef2531c2a92d1f74ac313f46b2b81e5347bb50f
jcbusche <jcbusche@gmail.com>
2013-12-25 23:04:40 -0500

    variable issues

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -60,7 +60,7 @@
 			    </time>
 			    <form>
 			    				            
-			        <input type = 'button' id = 'display-btn'  value = 'READ!' onclick = 'reader($toread)'/>
+			        <input type = 'button' id = 'display-btn'  value = 'READ!' onclick = 'reader("<?php echo $toread?>")'/>
 			        <br>
 				</form>
 				<form action="/readings/r_delete/<?=$post['post_id']?>" method="post">
