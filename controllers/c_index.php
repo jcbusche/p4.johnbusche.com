bd5db068c77343ce80030e7a7b232f9b1fba8fdf
jcbusche <jcbusche@gmail.com>
2013-12-26 23:28:25 -0500

    Final check

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -16,22 +16,13 @@ class index_controller extends base_controller {
 		
 		# Any method that loads a view will commonly start with this
 		# First, set the content of the template with a view file
-			$this->template->content = View::instance('v_index_index');
+		$this->template->content = View::instance('v_index_index');
 			
 		# Now set the <title> tag
-			$this->template->title = APP_NAME;
-	
-		# CSS/JS includes
-			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
+		$this->template->title = APP_NAME;
 	      					     		
 		# Render the view
-			echo $this->template;
+		echo $this->template;
 
 	} # End of method
 	
--- controllers/c_users.php
+++ controllers/c_users.php
@@ -3,16 +3,13 @@ class users_controller extends base_controller {
 
     public function __construct() {
         parent::__construct();
-        //echo "users_controller construct called<br><br>";
     } 
 
     public function index() {
-        #echo "This is the index page";
 
     }
 
     public function signup() {
-        #echo "This is the signup page";
         # Setup view
         $this->template->content = View::instance('v_users_signup');
         $this->template->title   = "Sign Up";
